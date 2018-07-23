022349c4d2b0fe2bfda116883db7f621db30b4ba
thedumbterminal <github@thedumbterminal.co.uk>
2018-07-23 23:16:19 +0100

    now supports required fields

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.0.2 (23/07/2018)
+
+* Required fields supported.
+
 ## v0.0.1 (21/07/2018)
 
 * Initial release.
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const jsonSchemaBigquery = module.exports = {}
 
 //Json schema on the left, bigquery on the right
 const typeMapping = {
-	'string': 'string',
-	'boolean': 'boolean'
+	'string': 'STRING',
+	'boolean': 'BOOLEAN',
+	'date-time': 'TIMESTAMP',
+	'integer': 'INTEGER',
+	'number': 'FLOAT'
 }
 
 jsonSchemaBigquery.convert = (jsonSchema) => {
@@ -12,33 +15,39 @@ jsonSchemaBigquery.convert = (jsonSchema) => {
 	}
 	return {
 		schema: {
-			fields: jsonSchemaBigquery._convertProperties(jsonSchema.properties)
+			fields: jsonSchemaBigquery._convertProperties(jsonSchema.properties, jsonSchema.required)
 		}
 	}
 }
 
 jsonSchemaBigquery._isComplex = (schema) => schema.type === 'object'
 
-jsonSchemaBigquery._convertProperties = (schema) => {
+jsonSchemaBigquery._convertProperties = (schema, required = []) => {
 	return Object.keys(schema).map((item) => {
 		if(jsonSchemaBigquery._isComplex(schema[item])){
 			return jsonSchemaBigquery._convertComplexProperty(item, schema[item])
 		}
-		return jsonSchemaBigquery._convertProperty(item, schema[item])
+		const initialMode = jsonSchemaBigquery._initialMode(required, item)
+		return jsonSchemaBigquery._convertProperty(item, schema[item], initialMode)
 	})
 }
 
-jsonSchemaBigquery._convertProperty = (name, value) => {
+jsonSchemaBigquery._initialMode = (required, field) => {
+	return required.includes(field) ? 'REQUIRED' : 'NULLABLE'
+}
+
+jsonSchemaBigquery._convertProperty = (name, value, mode) => {
 	return {
 		name: name,
-		type: typeMapping[value.type]
+		type: typeMapping[value.type],
+		mode: mode
 	}
 }
 
 jsonSchemaBigquery._convertComplexProperty = (name, contents) => {
 	return {
 		name: name,
-		type: 'record',
+		type: 'RECORD',
 		fields: jsonSchemaBigquery._convertProperties(contents.properties)
 	}
 }
