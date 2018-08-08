c01970d15c64220a3e48921434fe182a8fc95424
thedumbterminal <github@thedumbterminal.co.uk>
2018-08-08 09:21:23 +0100

    first example working

--- src/merger.js
+++ src/merger.js
@@ -1,5 +1,17 @@
+const _ = require('lodash');
 const merger = module.exports = {}
 
 merger.merge = (schema) => {
-	return schema.allOf.shift()
+	let combined = {}
+	schema.allOf.forEach((item) => {
+		combined = _.assignWith(combined, item, merger._mergeProperties)
+	})
+	return combined
+}
+
+merger._mergeProperties = (dst, src) => {
+	if(typeof dst === 'object' && typeof src === 'object'){
+		return Object.assign({}, dst, src)
+	}
+	return undefined
 }
