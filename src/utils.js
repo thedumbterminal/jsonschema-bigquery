1e27ac82f18a405b6d13021f09894ce09c307920
The Dumb Terminal <github@thedumbterminal.co.uk>
2022-08-05 17:46:43 +0100

    updating prettier config

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
-const util = require("util");
-const utils = (module.exports = {});
+const util = require('util')
+const utils = (module.exports = {})
 
 // Expected format is "id": "/events/server/ecommerce/v5.1.5/schema.json"
 utils.get_table_id = (id) => {
-  const idArr = id.split("/").slice(2, -1);
-  const version = idArr.pop();
-  return idArr.join("_") + "_" + version.split(".")[0];
-};
+  const idArr = id.split('/').slice(2, -1)
+  const version = idArr.pop()
+  return idArr.join('_') + '_' + version.split('.')[0]
+}
 
 utils.inspector = (obj) =>
   util.inspect(obj, {
@@ -14,4 +14,4 @@ utils.inspector = (obj) =>
     colors: process.stdout.isTTY, // Add colours if in an interactive terminal
     compact: false,
     breakLength: Infinity,
-  });
+  })
